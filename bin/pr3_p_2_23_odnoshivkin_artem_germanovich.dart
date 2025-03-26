138fbdb24a8d1b857179714260bbf7b4f5576f76
Twelve <limelaimov4567@gmail.com>
2025-03-27 00:58:26 +0300

    project

---
+++ bin/pr3_p_2_23_odnoshivkin_artem_germanovich.dart
@@ -0,0 +1,5 @@
+import 'package:pr3_p_2_23_odnoshivkin_artem_germanovich/pr3_p_2_23_odnoshivkin_artem_germanovich.dart' as pr3_p_2_23_odnoshivkin_artem_germanovich;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pr3_p_2_23_odnoshivkin_artem_germanovich.calculate()}!');
+}
---
+++ test/pr3_p_2_23_odnoshivkin_artem_germanovich_test.dart
@@ -0,0 +1,8 @@
+import 'package:pr3_p_2_23_odnoshivkin_artem_germanovich/pr3_p_2_23_odnoshivkin_artem_germanovich.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
